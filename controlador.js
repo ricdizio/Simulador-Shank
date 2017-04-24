3c04f071ae73bc14387ea9479506781c5a1114b4
Ricardo Di Zio <11-11274@usb.ve>
2017-04-24 15:44:50 -0400

    Reparado boton de borrar

--- controlador.js
+++ controlador.js
@@ -64,6 +64,29 @@ function actualizar(){
 function borrarLaberinto() {
     alert("Se limpiará el maze en pantalla");
     setup();
+    frameRate(0.8);
+    enabler=1;
+    X = x;
+    Y = y;
+    destino = [ [X/2-1,Y/2-1],[X/2,Y/2-1],[X/2-1,Y/2],[X/2,Y/2] ];
+
+    // coordenada de inicio
+    var inicio = new coord();
+    inicio.x = 0;
+    inicio.y = 0;
+
+    // Cardinal
+    var cardinalGlobal = 4;
+    //var cardinales= ['north','south','east','west'];
+
+    //Coordenada actual global
+    var coord_actual_global = new coord();
+    coord_actual_global.x = 0;
+    coord_actual_global.y = 0;
+    globalCoord.x = 0;
+    globalCoord.y = 0;
+    globalEnd.x = 0;
+    globalEnd.y = 0;
 }
 
 function abrirConsola(){
